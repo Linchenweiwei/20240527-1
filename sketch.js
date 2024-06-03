33c77b1605692275ebb5e75015599bbf5c89b9a3
Linchenweiwei <150638876+Linchenweiwei@users.noreply.github.com>
2024-06-03 11:47:51 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -69,11 +69,6 @@ function drawSkeleton() {
   for (let i = 0; i < poses.length; i++) {
     pose = poses[i];
 
-    push()
-    textSize(40)
-    scale(-1,1)
-    text("412730268，林宸瑋",partA.x-width,partA.y-150)
-
     // 繪製肩膀到手腕的線條
     for (let j = 5; j < 9; j++) {
       if (pose.keypoints[j].score > 0.1 && pose.keypoints[j + 2].score > 0.1) {
@@ -117,7 +112,7 @@ function drawSkeleton() {
       image(img, rightShoulder.x - 25, rightShoulder.y - 25, 50, 50); // 繪製圖片
     }
 
-     // 在頭頂上方顯示學號和姓名
+    // 在頭頂上方顯示學號和姓名
     let nose = pose.keypoints[0];
     if (nose.score > 0.1) {
       fill(255, 0, 0); // 設置填充顏色為紅色
